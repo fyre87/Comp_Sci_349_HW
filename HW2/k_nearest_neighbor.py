1bfe192c8666628334c9f1191311cd7457b5964a
anniejchu <chuannie4@gmail.com>
2023-10-24 16:14:05 -0500

    adding updated HW2 folder

---
+++ HW2/k_nearest_neighbor.py
@@ -0,0 +1,148 @@
+import numpy as np 
+import math
+# from starter import euclidean, cosim
+# from distance import euclidean_distances, manhattan_distances
+
+# returns Euclidean distance between vectors a dn b
+def euclidean(a,b):
+    # da = [int(i) for i in a]
+    # db = [int(i) for i in b]
+
+    #using math
+    # dist1 = math.dist(a, b)
+
+    # #using numpy
+    d1, d2 = np.array(a), np.array(b)
+    dist2 = np.sqrt(np.sum((d1 - d2)**2))
+
+    #naive
+    dist = math.sqrt(sum([(a_x - b_x)**2 for a_x, b_x in zip(a,b)]))
+    # print(dist, type(dist))
+    # print(dist1, type(dist1))
+    # print(dist2, type(dist2))
+
+    return(dist)
+        
+# returns Cosine Similarity between vectors a dn b
+def dot(x,y):
+    return sum(x1*x2 for x1,x2 in zip(x,y))
+def cosim(a,b):
+    #naive
+    dist = dot(a,b) / (math.sqrt(dot(a,a)) * math.sqrt(dot(b,b)))
+
+    #check vs numpy
+    dist1 = np.dot(a, b)/(np.linalg.norm(a)*np.linalg.norm(b))
+    # print(dist,dist1)
+    return(dist)
+
+class KNearestNeighbor():    
+    def __init__(self, n_neighbors, distance_measure='euclidean', aggregator='mode'):
+        """
+        K-Nearest Neighbor is a straightforward algorithm that can be highly
+        effective. Training time is...well...is there any training? At test time, labels for
+        new points are predicted by comparing them to the nearest neighbors in the
+        training data.
+
+        ```distance_measure``` lets you switch between which distance measure you will
+        use to compare data points. The behavior is as follows:
+
+        If 'euclidean', use euclidean_distances, if 'manhattan', use manhattan_distances.
+
+        ```aggregator``` lets you alter how a label is predicted for a data point based 
+        on its neighbors. If it's set to `mean`, it is the mean of the labels of the
+        neighbors. If it's set to `mode`, it is the mode of the labels of the neighbors.
+        If it is set to median, it is the median of the labels of the neighbors. If the
+        number of dimensions returned in the label is more than 1, the aggregator is
+        applied to each dimension independently. For example, if the labels of 3 
+        closest neighbors are:
+            [
+                [1, 2, 3], 
+                [2, 3, 4], 
+                [3, 4, 5]
+            ] 
+        And the aggregator is 'mean', applied along each dimension, this will return for 
+        that point:
+            [
+                [2, 3, 4]
+            ]
+
+        Arguments:
+            n_neighbors {int} -- Number of neighbors to use for prediction.
+            distance_measure {str} -- Which distance measure to use. Can be one of
+                'euclidean' or 'manhattan'. This is the distance measure
+                that will be used to compare features to produce labels. 
+            aggregator {str} -- How to aggregate a label across the `n_neighbors` nearest
+                neighbors. Can be one of 'mode', 'mean', or 'median'.
+        """
+        self.n_neighbors = n_neighbors
+        self.distance_measure = distance_measure
+        self.aggregator = aggregator
+        self.features = None #initialize with fit()
+        self.labels = None # intialize with fit()
+
+    def fit(self, features, targets):
+        """Fit features, a numpy array of size (n_samples, n_features). For a KNN, this
+        function should store the features and corresponding targets in class 
+        variables that can be accessed in the `predict` function. Note that targets can
+        be multidimensional! 
+        
+        Arguments:
+            features {np.ndarray} -- Features of each data point, shape of (n_samples,
+                n_features).
+            targets {[type]} -- Target labels for each data point, shape of (n_samples, 
+                n_dimensions).
+        """
+        self.features = np.where(features <=128, 0, 1)
+        self.labels = targets        
+
+    def predict_help(self, example_features):
+        """uses knn to classify ONE example
+        returns mode label 
+        """
+        distList = []
+        
+        for i in range(len(self.features)):
+            if self.distance_measure == 'euclidean':
+                distList.append((self.labels[i], euclidean(self.features[i], example_features)))
+            elif self.distance_measure == 'cosim':
+                distList.append((self.labels[i], cosim(self.features[i], example_features)))
+            #sorting by lowest distance x[1]
+            distList.sort(key = lambda x: x[1])
+            #getting just the labels i[0], distances are i[1]
+            k_nns = [i[0] for i in distList[:self.n_neighbors]]
+        
+        # if self.aggregator == 'mode':
+        #     probLabel = max(set(k_nns), key = k_nns.count) # 3
+        # elif self.aggregator == 'median':
+        #     probLabel = np.median(k_nns, axis = 0)
+        # elif self.aggregator == 'mean':
+        #     probLabel = np.mean(k_nns, axis = 1)
+        probLabel = max(set(k_nns), key = k_nns.count)
+        return probLabel
+    
+    def predict(self, query_features, ignore_first=False):
+        """Predict from features, a numpy array of size (n_samples, n_features) Use the
+        training data to predict labels on the test features. For each testing sample, compare it
+        to the training samples. Look at the self.n_neighbors closest samples to the 
+        test sample by comparing their feature vectors. The label for the test sample
+        is the determined by aggregating the K nearest neighbors in the training data.
+
+        Note that when using KNN for imputation, the predicted labels are the imputed testing data
+        and the shape is (n_samples, n_features).
+
+        Arguments:
+            features {np.ndarray} -- Features of each data point, shape of (n_samples,
+                n_features).
+            ignore_first {bool} -- If this is True, then we ignore the closest point
+                when doing the aggregation. This is used for collaborative
+                filtering, where the closest point is itself and thus is not a neighbor. 
+                In this case, we would use 1:(n_neighbors + 1).
+
+        Returns:
+            labels {np.ndarray} -- Labels for each data point, of shape (n_samples,
+                n_dimensions). This n_dimensions should be the same as n_dimensions of targets in fit function.
+        """
+        labels = [self.predict_help(i) for i in query_features]
+        return labels
+
+
---
+++ HW2/starter.py
@@ -0,0 +1,181 @@
+import math
+import numpy as np
+import csv 
+from random import sample
+from k_nearest_neighbor import KNearestNeighbor
+
+# # returns Euclidean distance between vectors a dn b
+# def euclidean(a,b):
+#     # da = [int(i) for i in a]
+#     # db = [int(i) for i in b]
+
+#     #using math
+#     # dist1 = math.dist(a, b)
+
+#     # #using numpy
+#     d1, d2 = np.array(a), np.array(b)
+#     dist2 = np.sqrt(np.sum((d1 - d2)**2))
+
+#     #naive
+#     dist = math.sqrt(sum([(a_x - b_x)**2 for a_x, b_x in zip(a,b)]))
+#     # print(dist, type(dist))
+#     # print(dist1, type(dist1))
+#     # print(dist2, type(dist2))
+
+#     return(dist)
+        
+# # returns Cosine Similarity between vectors a dn b
+# def dot(x,y):
+#     return sum(x1*x2 for x1,x2 in zip(x,y))
+# def cosim(a,b):
+#     #naive
+#     dist = dot(a,b) / (math.sqrt(dot(a,a)) * math.sqrt(dot(b,b)))
+
+#     #check vs numpy
+#     dist1 = np.dot(a, b)/(np.linalg.norm(a)*np.linalg.norm(b))
+#     # print(dist,dist1)
+#     return(dist)
+
+
+
+
+
+# #helper function, given 1 example (query_i), what are the k nearest neighbors' LABELS?
+# def getKnns(trainSet, query_i, k, metric):
+#     distList = [] #append tuples of label, distance computed
+#     for label, val in trainSet:
+#         if metric == 'euclidean':
+#             distList.append((label, euclidean(val, query_i[1])))
+#         elif metric == 'cosim':
+#             distList.append((label, cosim(val, query_i[1])))
+#     #sorting by lowest distance x[1]
+#     distList.sort(key = lambda x: x[1])
+#     #getting just the labels i[0], distances are i[1]
+#     k_nns = [i[0] for i in distList[:k]]
+#     return k_nns
+
+# #helper: given k nearest neighbors, what is the probability of the query_i's class
+# def mostprobLabel(k_nns):
+#     probLabel = max(set(k_nns), key = k_nns.count) # 3
+#     return probLabel
+
+
+def splitData(data, num_obs = 200):
+    #input: data is a filename
+    d = read_data_COMP(data)
+    labels =  [item[0] for item in d[:num_obs]]
+    features = [item[1] for item in d[:num_obs]]
+    npLabels = np.asarray(labels)
+    npFeatures = np.asarray(features)
+    return npLabels, npFeatures
+
+# returns a list of labels for the query dataset based upon labeled observations in the train dataset.
+# metric is a string specifying either "euclidean" or "cosim".  
+# All hyper-parameters should be hard-coded in the algorithm.
+def knn(train,query,metric):
+    #input: train & query are filenames
+    knn = KNearestNeighbor(5, metric)
+
+    t_labels, t_features = splitData(train)
+    print(len(t_labels))
+    knn.fit(t_features, t_labels)
+    q_labels, q_features = splitData(query)
+    return knn.predict(q_features)
+
+    #hyper-parameters
+    # k = 3
+    # num_obs = 300
+    # actual_labels = []
+    # labels = []
+    # for q in query:
+    #     nns = getKnns(train, q, k, metric)
+    #     q_label = mostprobLabel(nns)
+    #     actual_labels.append(q[0])
+    #     labels.append(q_label)
+    # print(actual_labels, labels)
+
+# returns a list of labels for the query dataset based upon observations in the train dataset. 
+# labels should be ignored in the training set
+# metric is a string specifying either "euclidean" or "cosim".  
+# All hyper-parameters should be hard-coded in the algorithm.
+def kmData(data_set):
+    km_D = [item[1] for item in data_set]
+    return km_D
+
+def kmeans(train,query,metric):
+    k = 3
+    max_iters = 3
+
+    #initiating centroids randomly
+    centroids = sample(train, k)
+
+    for i in range(max_iters):
+        groups = {}
+        distList = []
+        for i in range(k):
+            groups[i] = []
+        for t in train:
+            distList = [euclidean(t, centroids[c]) for c in centroids]
+            t_Group = distList.index(min(distList))
+            groups[t_Group].append(t)
+
+    return(labels)
+
+def read_data(file_name):
+    data_set = []
+    with open(file_name,'rt') as f:
+        for line in f:
+            line = line.replace('\n','')
+            tokens = line.split(',')
+            label = tokens[0]
+            attribs = []
+            for i in range(784):
+                attribs.append(tokens[i+1])
+            data_set.append([label,attribs])
+    return(data_set)
+        
+def read_data_COMP(file_name):
+    data_set = []
+    c = 0
+    with open(file_name,'rt') as f:
+        for line in f:
+            c+=1
+            line = line.replace('\n','')
+            tokens = line.split(',')
+            label = int(tokens[0])
+            attribs = []
+            for i in range(784):
+                attribs.append(int(tokens[i+1]))
+            data_set.append([label,attribs])
+    return(data_set)
+
+def show(file_name,mode):
+    data_set = read_data(file_name)
+    for obs in range(len(data_set)):
+        for idx in range(784):
+            if mode == 'pixels':
+                if data_set[obs][1][idx] == '0':
+                    print(' ',end='')
+                else:
+                    print('*',end='')
+            else:
+                print('%4s ' % data_set[obs][1][idx],end='')
+            if (idx % 28) == 27:
+                print(' ')
+        print('LABEL: %s' % data_set[obs][0],end='')
+        print(' ')
+            
+def main():
+
+    train = 'train.csv'
+    query = 'test.csv'
+    res = knn(train, query, 'euclidean')
+    print(res)
+    # D = read_data_COMP('train.csv')
+    # features = [item[1] for item in D]
+    # print(features)
+    # show('valid.csv','pixels')
+    
+if __name__ == "__main__":
+    main()
+    
